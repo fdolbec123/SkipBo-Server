fc0f6d6e2d73d0234c3da376247117b2f0fcad61
Francis Dolbec <fdolbec123@gmail.com>
2020-06-08 22:03:19 -0400

    Revert back to what is working after the 5 parts video tutorial.

--- client-test.py
+++ client-test.py
@@ -1,11 +1,49 @@
 import socket
+import sys
+import select
+import  errno
 import pickle
 
 HEADERSIZE = 10
 
 socket_de_connexion = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-socket_de_connexion.connect((socket.gethostname(), 5555))
-
+socket_de_connexion.connect((socket.gethostname(), 5556))
+# my_username = input("Username: ")
+# socket_de_connexion.setblocking(False)
+# username = my_username.encode("utf-8")
+# username_header = f"{len(username):<{HEADERSIZE}}".encode("utf-8")
+# socket_de_connexion.send(username_header + username)
+#
+# while True:
+#     data = input(f"{my_username} > ")
+#     if data:
+#         data = data.encode("utf-8")
+#         data_header = f"{len(data) :< {HEADERSIZE}}".encode("utf-8")
+#         socket_de_connexion.send(data_header + data)
+#     try:
+#         while True:
+#             username_header = socket_de_connexion.recv(HEADERSIZE)
+#             if not len(username_header):
+#                 print("Connexion fermée par le server")
+#                 sys.exit()
+#             username_length = int(username_header.decode("utf-8"))
+#             username = socket_de_connexion.recv(username_length).decode("utf-8")
+#             data_header = socket_de_connexion.recv(HEADERSIZE)
+#             data_length = int(data_header.decode("utf-8"))
+#             data = socket_de_connexion.recv(data_length).decode("utf-8")
+#
+#             print(f"{username} > {data}")
+#     except IOError as e:
+#         if e.errno != errno.EAGAIN and e.errno != errno.EWOULDBLOCK:
+#             print("Reading error", str(e))
+#             sys.exit()
+#         continue
+#
+#     except Exception as  e:
+#         print("General error", str(e))
+#         sys.exit()
+#
+#
 while True:
     full_msg = b""
     new_msg = True
--- server2.py
+++ server2.py
@@ -1,11 +1,64 @@
 import socket
+import select
 import pickle
 
 HEADERSIZE = 10
 
 socket_de_connexion = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-socket_de_connexion.bind((socket.gethostname(), 5555))
+socket_de_connexion.bind((socket.gethostname(), 5556))
+# socket_de_connexion.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 socket_de_connexion.listen(5)
+
+# sockets_list = [socket_de_connexion]
+#
+# clients = {}
+#
+# # ---------------------------------------------------
+#
+#
+# def receive_data(client_socket):
+#     try:
+#         data_header = client_socket.recv(HEADERSIZE)
+#         if not len(data_header):
+#             return False
+#         data_length = int(data_header.decode("utf-8"))
+#         return {"header": data_header, "data": client_socket.recv(data_length)}
+#     except:
+#         return False
+#
+#
+# while True:
+#     read_sockets, _, exception_sockets = select.select(sockets_list, [], sockets_list)
+#
+#     for notified_socket in read_sockets:
+#         if notified_socket == socket_de_connexion:
+#             client_socket, client_address = socket_de_connexion.accept()
+#             user = receive_data(client_socket)
+#             if user is False:
+#                 continue
+#             sockets_list.append(client_socket)
+#             clients[client_socket] = user
+#             print(f"Nouvelle connexion de {client_address[0]}:{client_address[1]} username:{user['data'].decode('utf-8')}")
+#         else:
+#             data = receive_data(notified_socket)
+#             if data is False:
+#                 print(f"Connexion fermée de {clients[notified_socket]['data'].decode('utf-8')}")
+#                 sockets_list.remove(notified_socket)
+#                 del clients[notified_socket]
+#                 continue
+#             user = clients[notified_socket]
+#             print(f"Données provenant de {user['data'].decode('utf-8')}:{data['data'].decode('utf-8')}")
+#
+#             for client_socket in clients:
+#                 if client_socket != notified_socket:
+#                     socket_de_connexion.send(user['header'] + user['data'] + data['header'] + data['data'])
+#
+#     for notified_socket in exception_sockets:
+#         sockets_list.remove(notified_socket)
+#         del clients[notified_socket]
+
+# --------------------------------------------------
+
 
 while True:
     clientsocket, address = socket_de_connexion.accept()
